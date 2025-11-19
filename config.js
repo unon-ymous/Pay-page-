91e5316556b52c63a7453e0bda8eb61602a8ee3e
All-Courses <instantmatch0@gmail.com>
2025-11-19 21:31:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PAY_CONFIG = {
-  Qrlink: "https://envs.sh/-u0.jpg",
-  Upiid: "paytm.s1wq74t@pty"
+  Qrlink: "https://branch-panel-data.s3.ap-south-1.amazonaws.com/images/qr_code/1760690156.jpeg",
+  Upiid: "gpay-11237583818@okbizaxis"
 };
