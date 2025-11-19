8a8a20e6976b6712b97b0ed02ef707585118c80d
All-Courses <instantmatch0@gmail.com>
2025-11-20 01:24:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 // Edit ONLY these two values (Qrlink and Upiid).
 // Keep the key names and capitalization exactly as-is (Qrlink, Upiid).
 
-window.PAY_CONFIG = {
-  Qrlink: "https://i.ibb.co/84fYnMr8/1756799447.jpg",           // e.g. "/assets/qr.png"  or "https://yourdomain.com/qr.png"
-  Upiid: "paytm.s1w81n4@pty"             // e.g. "merchant@upi"
+window.CONFIG = {
+  qrlink: "https://i.ibb.co/84fYnMr8/1756799447.jpg",           // e.g. "/assets/qr.png"  or "https://yourdomain.com/qr.png"
+  upiid: "paytm.s1w81n4@pty"             // e.g. "merchant@upi"
 };
 
