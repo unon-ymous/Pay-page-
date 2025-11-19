fcba2c99236d782bce18a410c54695d64e5de859
All-Courses <instantmatch0@gmail.com>
2025-11-19 22:41:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
 
 window.PAY_CONFIG = {
   Qrlink: "https://i.ibb.co/84fYnMr8/1756799447.jpg",           // e.g. "/assets/qr.png"  or "https://yourdomain.com/qr.png"
-  Upiid: "paytm.s1r0bff@pty"             // e.g. "merchant@upi"
+  Upiid: "paytm.s1w81n4@pty"             // e.g. "merchant@upi"
 };
 
