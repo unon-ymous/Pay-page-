d908746c5ec944d63577700a9f5d032c4ea6f5bd
All-Courses <instantmatch0@gmail.com>
2025-11-20 01:40:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // Keep the key names and capitalization exactly as-is (Qrlink, Upiid).
 
 window.CONFIG = {
-  qrlink: "https://i.ibb.co/20NTn6by/1760690156.jpg",           // e.g. "/assets/qr.png"  or "https://yourdomain.com/qr.png"
-  upiid: "paytm.s1tknyt@pty"             // e.g. "merchant@upi"
+  qrlink: "https://i.ibb.co/m59ft7mp/1762438569.jpg",           // e.g. "/assets/qr.png"  or "https://yourdomain.com/qr.png"
+  upiid: "paytm.s1wq74t@pty"             // e.g. "merchant@upi"
 };
 
