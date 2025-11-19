b4dd531356002b0131f2ef32b130b78840ff04c7
All-Courses <instantmatch0@gmail.com>
2025-11-20 02:10:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
+// config.js
 window.CONFIG = {
-  qrlink: "https://envs.sh/-Qy.jpg",
-  upiid: "paytm.s1tbcvt@pty"
+  qrlink: "https://i.ibb.co/gFPbGzZH/20251118-155331.jpg",
+  upiid: "merchant@upi",
+  carousel: [
+    "https://i.ibb.co/gFPbGzZH/20251118-155331.jpg",
+    "https://i.ibb.co/TBTd5S3p/20251118-160648.jpg",
+    "https://i.ibb.co/fVJnf9V7/20251118-153613.jpg"
+  ],
+  contactLink: "https://wa.me/919999999999",
+  contactText: "Contact Admin"
 };
-
